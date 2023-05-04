f0e5d026aff0450d3cef33752b232d32790264b2
Thomas Guerrero <thomaseguerrero@gmail.com>
2023-05-04 18:48:19 -0400

    Delete bread

--- controllers/bread.js
+++ controllers/bread.js
@@ -9,12 +9,37 @@ router.get('/', (req, res) => {
 })
 
 //GET bread a specific bread by index
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
 router.get('/:index', (req, res) => {
     const  { index } = req.params
     res.render('show', {
-        bread: Bread[index]
+        bread: Bread[index],
+        index: index
     })
-    // res.send(Bread[index])
 })
+
+
+
+router.post('/', (req, res) => {
+    if (!req.body.image) req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true 
+    } else {
+        req.body.hasGluten = false
+    }
+
+    Bread.push(req.body)
+    res.status(303).redirect('/breads')
+})
+
+router.delete('/:index', (req, res) => {
+    const { index } = req.params
+    Bread.splice(index, 1)
+    res.status(303).redirect('/breads')
+}) 
+
 
 module.exports = router
--- views/show.jsx
+++ views/show.jsx
@@ -1,10 +1,11 @@
 const React  = require('react')
 const Default = require('./layouts/default')
 
-function Show({ bread }) {
+function Show({ bread, index }) {
     console.log(bread)
     return (
         <Default>
+            
             <h3>{bread.name}</h3>
             <p>
                 and it 
@@ -15,9 +16,12 @@ function Show({ bread }) {
                 have gluten
             </p>
             <img src={bread.image} alt={bread.name}></img>
+            <form action={`/breads/${index}?_method=DELETE`} method='POST'>
+                <input type='submit' value='DELETE' />
+            </form>
             <li>
                 <a href={'/breads'}>Go home</a>
-                </li>
+            </li>
         </Default>
     )
 }
