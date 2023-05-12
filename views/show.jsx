7e87466cd032341e345ffcde638fc67fdb5652fa
Thomas Guerrero <thomaseguerrero@gmail.com>
2023-05-11 20:20:05 -0400

    Added bakers

--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React  = require('react')
 const Default = require('./layouts/default')
 
-function Show({ bread, index }) {
+function Show({ bread }) {
     return (
         <Default>
             <h3>{bread.name}</h3>
@@ -14,10 +14,11 @@ function Show({ bread, index }) {
                 have gluten
             </p>
             <img src={bread.image} alt={bread.name}></img>
-            <a href={`/breads/${index}/edit`}>
+            <p>{bread.getBakedBy()}</p>
+            <a href={`/breads/${bread.id}/edit`}>
                 <button>edit</button>
                 </a>
-            <form action={`/breads/${index}?_method=DELETE`} method='POST'>
+            <form action={`/breads/${bread.id}?_method=DELETE`} method='POST'>
                 <input type='submit' value='DELETE' />
             </form>
             <li>
